ebda4c5bd00e566102a9ce208626af631e3bd6bd
aniket010011 <149388350+aniket010011@users.noreply.github.com>
2025-12-13 02:04:55 +0530

    Update model file names and maintain functionality

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import joblib
 # -----------------------------
 @st.cache_resource
 def load_models():
-    classifier = joblib.load("best_classifier_LGBMClassifier_py311.pkl")
-    regressor = joblib.load("best_regressor_LinearRegression_py311.pkl")
+    classifier = joblib.load("best_classifier_LGBMClassifier_py313.pkl")
+    regressor = joblib.load("best_regressor_LinearRegression_py313.pkl")
     return classifier, regressor
 
 classifier, regressor = load_models()
@@ -82,6 +82,7 @@ if st.button("Predict"):
         st.write(f"Probability of GOOD investment: **{prob:.2f}**")
     except:
         pass
+
 
 
 
